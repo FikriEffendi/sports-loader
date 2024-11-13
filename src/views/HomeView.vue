f55d6961ef38f28c025e461a8655337a599f59b0
aerochops <150325960+aerochops@users.noreply.github.com>
2024-11-13 13:46:35 +0700

    setting awal vue dan inisiasi gambar

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="text-2xl font-bold underline">Halo</div>
+</template>
+
+<script setup>
+const images = [
+  {
+    src: "/public/baseball-svgrepo-com.svg",
+    alt: "",
+  },
+];
+</script>
+
+<style lang="scss" scoped></style>
