adbd953cf63a2765d7c886d0e3ceb57454d680be
aerochops <150325960+aerochops@users.noreply.github.com>
2024-11-14 01:12:45 +0700

    layouting svg,text dan background

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,31 @@
 <template>
-  <div class="text-2xl font-bold underline">Halo</div>
+  <div class="bg-[#0f977f]">
+    <div
+      class="flex items-center justify-center min-h-screen"
+      v-for="image in images"
+    >
+      <div class="space-y-3">
+        <div class="flex justify-center">
+          <div class="text-xl font-bold text-white">Halo.</div>
+        </div>
+        <div class="w-[230px] h-[130px]">
+          <img
+            :src="image.src"
+            alt=""
+            class="w-full h-full"
+            ref="storeImages"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
+
+const storeImages = ref([]);
+
 const images = [
   {
     src: "/public/baseball-svgrepo-com.svg",
